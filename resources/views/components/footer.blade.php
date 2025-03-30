c486e82e8bde4960547f2c18d38f37394d48c72d
FelipeRey97 <andresfrey97@gmail.com>
2025-03-30 09:10:29 -0500

    finished header styles

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<p>Sabores en línea</p>
+<nav>
+    <ul>
+        <li><a href="{{ url('/') }}">Inicio</a></li>
+        <li><a href="{{ url('/recetas') }}">Recetas</a></li>
+        <li><a href="{{ url('/contacto') }}">Contacto</a></li>
+    </ul>
+</nav>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<h1>Sabores en Línea</h1>
+
+<nav class="navbar">
+    <ul>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Recetas del día</a></li>
+        <li><a href="">Categorías</a></li>
+        <li><a href="">Nosotros</a></li>
+        <li><a href="">Ingresar</a></li>
+        <li><a href="">Registrarse</a></li>
+    </ul>
+</nav>
