2abb62d49034bbaac1ed5dc1f043d770d489ea66
FelipeRey97 <andresfrey97@gmail.com>
2025-03-30 11:38:46 -0500

    Finished footer styles

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,20 @@
-<p>Sabores en línea</p>
-<nav>
-    <ul>
-        <li><a href="{{ url('/') }}">Inicio</a></li>
-        <li><a href="{{ url('/recetas') }}">Recetas</a></li>
-        <li><a href="{{ url('/contacto') }}">Contacto</a></li>
-    </ul>
-</nav>
+
+
+    <section class="footerSection">
+        <h2>Sabores en línea</h2>
+        <ul class="socialNetworks">    
+            <li><a href=""><i class="fa-brands fa-square-facebook"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-square-youtube"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-square-instagram"></i></a></li>
+        </ul>
+    </section>
+
+    <nav class="navOptions">
+        <ul>
+            <li><a href="{{ url('/') }}">Inicio</a></li>
+            <li><a href="{{ url('/recetas') }}">Recetas</a></li>
+            <li><a href="{{ url('/contacto') }}">Contacto</a></li>
+        </ul>
+    </nav>
+
+<p> &copy; Sabores en Línea Todos los derechos reservados </p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <script src="https://kit.fontawesome.com/f243ce0afc.js" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -16,8 +17,8 @@
     @yield('content')
     </main>
 
-    <footer>
-    
+    <footer class="footer">
+    @include('components.footer')
     </footer>
 </body>
 </html>
